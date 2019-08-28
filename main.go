d2dac3913e0e211ea41daa5d752337d8e851c68e
Kompeko <bruno.ferreirasagomes@gmail.com>
2019-08-28 13:59:33 +0200

    added table, renamed variables

--- main.go
+++ main.go
@@ -5,8 +5,18 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
+type Trainee struct{
+	ID int
+	Surname string
+	Name string
+	Age int
+	TA string
+	QPA string
+	Project string
+}
 
 func main(){
 	/*
@@ -19,10 +29,24 @@ func main(){
 	name := flag.String("Name", "Alice", "set Name")
 	 */
 	people := []string{"Alice", "Bob"}
-	addPerson := flag.Bool("addPerson", false, "adds a person to the database")
-	showAll := flag.Bool("showAll", false, "display all elements")
+	var dummy Trainee
+	dummy.ID = 1
+	dummy.Surname = "Hellman"
+	dummy.Name = "Alice"
+	dummy.Age = 18
+	dummy.TA = "Diffie"
+	dummy.QPA = "Bob"
+	dummy.Project = "Encryption"
+
+
+	var trainees []Trainee
+	trainees = append(trainees, dummy)
+	addTrainee := flag.Bool("addTrainee", false, "adds a trainee to the database")
+	showAll := flag.Bool("showAll", false, "show all trainees in a table")
+	//showOne := flag.Int("showOne", -1, "show trainee with this ID")
+	//delete
 	flag.Parse()
-	if *addPerson{
+	if *addTrainee{
 		fmt.Println("please provide a name")
 		reader := bufio.NewReader(os.Stdin)
 		person,_ := reader.ReadString('\n')
@@ -30,11 +54,62 @@ func main(){
 		people = append(people, person)
 	}
 	if *showAll{
-		fmt.Println(people)
+		tableStart()
+		for _, trainee := range trainees {
+			tableContent(trainee)
+		}
+		tableEnd()
 	}
 
 }
 
+func tableStart(){
+	fmt.Println("+-----+---------------+---------------+----------+------------------+------------------+------------------+")
+	fmt.Println("| ID  | Nachname      | Vorname       | Alter    | Lernbegleiter    | QPA              | Projekt          |")
+	fmt.Println("+-----+---------------+---------------+----------+------------------+------------------+------------------+")
+}
+
+func tableEnd(){
+	fmt.Println("+-----+---------------+---------------+----------+------------------+------------------+------------------+")
+}
+
+func tableContent(trainee Trainee){
+		id := strconv.Itoa(trainee.ID)
+		age := strconv.Itoa(trainee.Age)
+		dataPoint := "| "
+		dataPoint += id
+		for i := len(dataPoint); i < 6; i++{
+			dataPoint += " "
+		}
+		dataPoint += "| " + trainee.Surname
+		for i := len(dataPoint); i < 22; i++{
+			dataPoint += " "
+		}
+		dataPoint += "| " + trainee.Name
+		for i := len(dataPoint); i < 38; i++{
+			dataPoint += " "
+		}
+		dataPoint += "| " + age
+		for i := len(dataPoint); i < 49; i++{
+			dataPoint += " "
+		}
+		dataPoint += "| " + trainee.TA
+		for i := len(dataPoint); i < 68; i++{
+			dataPoint += " "
+		}
+		dataPoint += "| " + trainee.QPA
+		for i := len(dataPoint); i < 87; i++{
+			dataPoint += " "
+		}
+		dataPoint += "| " + trainee.Project
+		for i := len(dataPoint); i < 106; i++{
+			dataPoint += " "
+		}
+		dataPoint += "|"
+		fmt.Println(dataPoint)
+
+
+}
 
 func bubbleSort(a []int) []int{
 	var i int = 1
